c1ad249348eda48819c6b682480669bc6c308de5
Sammy Owa <99755773+sammykb82@users.noreply.github.com>
2025-06-20 15:27:55 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 with st.sidebar: 
     st.set_page_config(page_title="RAG Question Answer ")
     st.header(" RAG Question Answer")
-    uploaded_file = st.file_upload(
+    uploaded_file = st.file_uploader(
         "** Upload PDF files for AnA**", type=["pdf"], accept_multiple_files=False
     )
     
