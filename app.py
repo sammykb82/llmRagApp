e5909757c1781518c1666be9162174bf9782b47d
Sammy Owa <99755773+sammykb82@users.noreply.github.com>
2025-06-20 15:26:03 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+with st.sidebar: 
+    st.set_page_config(page_title="RAG Question Answer ")
+    st.header(" RAG Question Answer")
+    uploaded_file = st.file_upload(
+        "** Upload PDF files for AnA**", type=["pdf"], accept_multiple_files=False
+    )
+    
+    process = st.tk.Button(
+        "Process"
+    )
